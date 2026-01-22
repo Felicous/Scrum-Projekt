74b715515de6bc80bf9b1f8e2e3e5e35894efacf
Max <maximilian.sgarz@informatik.uni-giessen.de>
2026-01-22 15:29:25 +0100

    addition term generation (others in progress)
    implemented Get methods

--- test/src/Term.java
+++ test/src/Term.java
@@ -16,9 +16,19 @@ public class Term {
 
         result = number_1 + number_2;
 
-        // int operator = r.nextInt(3)    // operator
+        // int operator = r.nextInt(3)    // operator will be implemented in later sprint
 
-
-
+    }
+    public int get_result(){
+        return result;
+    }
+    public int get_number_1(){
+        return number_1;
+    }
+    public int get_number_2(){
+        return number_2;
+    }
+    public char get_op(){
+        return op;
     }
 }
